834623e385809d1bcdb0e4f807e4a8dcf24f3cd4
Nikhil-Nishad <nikhilnishad1801@gmail.com>
2025-02-19 19:19:30 +0530

    Pattern_14

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -222,6 +222,19 @@ void pattern13(int n)
         cout << endl;
     }
 }
+void pattern14(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        char ch = 'A';
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << ch++ << " ";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     int n;
