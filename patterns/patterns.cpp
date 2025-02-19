e0eea68ea8298cc27cf39ffcd47555d902d720f5
Nikhil-Nishad <nikhilnishad1801@gmail.com>
2025-02-19 18:55:03 +0530

    Pattern_12

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -182,6 +182,34 @@ void pattern11(int n)
         cout << endl;
     }
 }
+void pattern12(int n){
+    int space = 2 * (n - 1);
+
+    for (int i = 1; i <= n; i++)
+    {
+
+        // number
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+
+        // spaces
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
+        }
+
+        // number
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+        space -= 2;
+    }
+}
+
 int main()
 {
     int n;
--- readme.md
+++ readme.md
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DSA Practice in C++</title>
-</head>
-<body>
 
 <h1>Data Structures and Algorithms Practice in C++</h1>
 
